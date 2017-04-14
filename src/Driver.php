f9745fc63d3538ba6dfc3e2d62ff62a21c37a5cb
Monster3D <monster3d@mail.ru>
2017-04-14 12:17:06 +0500

    Added use independent settings of driver

--- src/Driver.php
+++ src/Driver.php
@@ -1,5 +1,55 @@
 <?php
 
 class Driver extends Unirest\Request {
-    //pass
+
+    /**
+     *
+     * Driver constructor.
+     *
+     * @param array $settings
+     *
+     * @example array['verify_host' => true, 'verify_peer' => true, 'timeout' => 40]
+     *
+     */
+    public function __construct($settings = [])
+    {
+        $defaultSettings = $this->getDefaultSettings();
+        if (array_key_exists('verify_host', $settings)) {
+            $verifyHost = (bool)$settings['verify_host'];
+        } else {
+            $verifyHost = $defaultSettings['verify_host'];
+        }
+
+        if (array_key_exists('verify_peer', $settings)) {
+            $verifyPeer = (bool)$settings['verify_peer'];
+        } else {
+            $verifyPeer = $defaultSettings['verify_peer'];
+        }
+
+        if (array_key_exists('timeout', $settings)) {
+            $timeout = (int)$settings['timeout'];
+        } else {
+            $timeout = $defaultSettings['timeout'];
+        }
+
+        $this->verifyHost($verifyHost);
+        $this->verifyPeer($verifyPeer);
+        $this->timeout($timeout);
+    }
+
+    /**
+     *
+     * Настройки по умолчанию
+     *
+     * @return array
+     *
+     */
+    private function getDefaultSettings()
+    {
+        return [
+            'verify_host' => false,
+            'verify_peer' => false,
+            'timeout'     => 50
+        ];
+    }
 }
