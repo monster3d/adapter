f53ea7b14c6fd8a8d9007277d83036b823f031cf
Monster3D <monster3d@mail.ru>
2017-04-17 14:07:25 +0500

    Default argument in consructor not use array

--- src/Driver.php
+++ src/Driver.php
@@ -6,50 +6,15 @@ class Driver extends Unirest\Request {
      *
      * Driver constructor.
      *
-     * @param array $settings
-     *
-     * @example array['verify_host' => true, 'verify_peer' => true, 'timeout' => 40]
+     * @param $verifyHost bool Отключает проверку SSL для общего хоста
+     * @param $verifyPeer bool Отключает проверку SSL узла сети
+     * @param $timeout int Устанавливает время запроса
      *
      */
-    public function __construct($settings = [])
+    public function __construct($verifyHost = false, $verifyPeer = false, $timeout = 50)
     {
-        $defaultSettings = $this->getDefaultSettings();
-        if (array_key_exists('verify_host', $settings)) {
-            $verifyHost = (bool)$settings['verify_host'];
-        } else {
-            $verifyHost = $defaultSettings['verify_host'];
-        }
-
-        if (array_key_exists('verify_peer', $settings)) {
-            $verifyPeer = (bool)$settings['verify_peer'];
-        } else {
-            $verifyPeer = $defaultSettings['verify_peer'];
-        }
-
-        if (array_key_exists('timeout', $settings)) {
-            $timeout = (int)$settings['timeout'];
-        } else {
-            $timeout = $defaultSettings['timeout'];
-        }
-
         $this->verifyHost($verifyHost);
         $this->verifyPeer($verifyPeer);
         $this->timeout($timeout);
-    }
-
-    /**
-     *
-     * Настройки по умолчанию
-     *
-     * @return array
-     *
-     */
-    private function getDefaultSettings()
-    {
-        return [
-            'verify_host' => false,
-            'verify_peer' => false,
-            'timeout'     => 50
-        ];
     }
 }
